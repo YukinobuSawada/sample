b1b90cdae61971c015b5baef8c6a59021658015d
YukinobuSawada <spw.kyoka@gmail.com>
2020-05-24 13:16:00 +0900

    [Finish] 3-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5ebfd330fdb8307ae7f5b83955ccacd40b62434a82a4083627690ed684e23d47cf04873f3e8664ce31f06348f354b2e117878b736c514cfacd19ba0eff576e9e'
